334dae0e5737bb6ed00ec2a478a73d31c79653b8
Jan Buschtöns <jan.buschtoens@clark.de>
2018-11-20 13:42:37 +0100

    add reproduction

--- src/router.js
+++ src/router.js
@@ -1,11 +1,22 @@
-import EmberRouter from "@ember/routing/router";
-import config from "../config/environment";
+import EmberRouter from '@ember/routing/router';
+import config from '../config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route('foo', function() {
+    this.route('bar', function() {
+      // You don't even need to register a third-level route. Just providing the
+      // callback triggers the error.
+      // this.route('qux');
+    });
+  });
+  this.route('one', function() {
+    this.route('two');
+  });
+});
 
 export default Router;
---
+++ src/ui/routes/application/controller.js
@@ -0,0 +1,12 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+export default Controller.extend({
+  router: service(),
+
+  actions: {
+    transitionTo(...args) {
+      this.router.transitionTo(...args);
+    }
+  }
+});
